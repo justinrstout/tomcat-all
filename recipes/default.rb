d1350d3b293be2401c50e808d23abc4a7aaa72ff
Justin R. Stout <justin.stout@perceptivesoftware.com>
2015-06-02 13:28:47 -0500

    Add basic Windows support

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,78 +8,89 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# Build download URL
 tomcat_version = node['tomcat-all']['version']
 major_version = tomcat_version[0]
-download_url = "http://archive.apache.org/dist/tomcat/tomcat-#{major_version}/v#{tomcat_version}/bin/apache-tomcat-#{tomcat_version}.tar.gz"
 
-# Create group
-group node['tomcat-all']['group']
+if platform_family?('windows')
+  windows_package "Apache Tomcat #{major_version}.0" do
+    source "http://archive.apache.org/dist/tomcat/tomcat-#{major_version}/v#{tomcat_version}/bin/apache-tomcat-#{tomcat_version}.exe"
+  end
 
-# Create user
-user node['tomcat-all']['user'] do
+  service "Tomcat#{major_version}" do
+    action :start
+  end
+else
+  # Build download URL
+  download_url = "http://archive.apache.org/dist/tomcat/tomcat-#{major_version}/v#{tomcat_version}/bin/apache-tomcat-#{tomcat_version}.tar.gz"
+
+  # Create group
   group node['tomcat-all']['group']
-  system true
-  shell '/bin/bash'
-end
 
-# Download and unpack tomcat
-ark 'tomcat' do
-  url download_url
-  version node['tomcat-all']['version']
-  home_dir node['tomcat-all']['tomcat_home']
-  owner node['tomcat-all']['user']
-  group node['tomcat-all']['group']
-end
+  # Create user
+  user node['tomcat-all']['user'] do
+    group node['tomcat-all']['group']
+    system true
+    shell '/bin/bash'
+  end
 
-# Log rotation (catalina.out)
-template '/etc/logrotate.d/tomcat' do
-  source 'logrotate.conf.erb'
-  mode '0644'
-  owner node['tomcat-all']['user']
-  group node['tomcat-all']['group']
-end
+  # Download and unpack tomcat
+  ark 'tomcat' do
+    url download_url
+    version node['tomcat-all']['version']
+    home_dir node['tomcat-all']['tomcat_home']
+    owner node['tomcat-all']['user']
+    group node['tomcat-all']['group']
+  end
 
-# Tomcat server configuration
-template "#{node['tomcat-all']['tomcat_home']}/conf/server.xml" do
-  source 'server.conf.erb'
-  mode '0644'
-  owner node['tomcat-all']['user']
-  group node['tomcat-all']['group']
-end
+  # Log rotation (catalina.out)
+  template '/etc/logrotate.d/tomcat' do
+    source 'logrotate.conf.erb'
+    mode '0644'
+    owner node['tomcat-all']['user']
+    group node['tomcat-all']['group']
+  end
 
-# Tomcat catalina configuration
-template "#{node['tomcat-all']['tomcat_home']}/bin/setenv.sh" do
-  source 'setenv.sh.erb'
-  mode '0755'
-  owner node['tomcat-all']['user']
-  group node['tomcat-all']['group']
-end
+  # Tomcat server configuration
+  template "#{node['tomcat-all']['tomcat_home']}/conf/server.xml" do
+    source 'server.conf.erb'
+    mode '0644'
+    owner node['tomcat-all']['user']
+    group node['tomcat-all']['group']
+  end
 
-# Tomcat init script configuration
-template "/etc/init.d/tomcat#{major_version}" do
-  source 'init.conf.erb'
-  mode '0755'
-  owner node['tomcat-all']['user']
-  group node['tomcat-all']['group']
-end
+  # Tomcat catalina configuration
+  template "#{node['tomcat-all']['tomcat_home']}/bin/setenv.sh" do
+    source 'setenv.sh.erb'
+    mode '0755'
+    owner node['tomcat-all']['user']
+    group node['tomcat-all']['group']
+  end
 
-include_recipe 'tomcat-all::set_tomcat_home'
+  # Tomcat init script configuration
+  template "/etc/init.d/tomcat#{major_version}" do
+    source 'init.conf.erb'
+    mode '0755'
+    owner node['tomcat-all']['user']
+    group node['tomcat-all']['group']
+  end
 
-# Create default catalina.pid file to prevent restart error for 1st run of coookbook.
-file "#{node['tomcat-all']['tomcat_home']}/catalina.pid" do
-  owner node['tomcat-all']['user']
-  group node['tomcat-all']['group']
-  mode '0755'
-  action :create
-  not_if { ::File.exist?("#{node['tomcat-all']['tomcat_home']}/catalina.pid") }
-end
+  include_recipe 'tomcat-all::set_tomcat_home'
 
-# Enabling tomcat service and restart the service if subscribed template has changed.
-service "tomcat#{major_version}" do
-  supports :restart => true
-  action :enable
-  subscribes :restart, "template[/etc/init.d/tomcat#{major_version}]", :delayed
-  subscribes :restart, "template[#{node['tomcat-all']['tomcat_home']}/bin/setenv.sh]", :delayed
-  subscribes :restart, "template[#{node['tomcat-all']['tomcat_home']}/conf/server.xml]", :delayed
+  # Create default catalina.pid file to prevent restart error for 1st run of coookbook.
+  file "#{node['tomcat-all']['tomcat_home']}/catalina.pid" do
+    owner node['tomcat-all']['user']
+    group node['tomcat-all']['group']
+    mode '0755'
+    action :create
+    not_if { ::File.exist?("#{node['tomcat-all']['tomcat_home']}/catalina.pid") }
+  end
+
+  # Enabling tomcat service and restart the service if subscribed template has changed.
+  service "tomcat#{major_version}" do
+    supports :restart => true
+    action :enable
+    subscribes :restart, "template[/etc/init.d/tomcat#{major_version}]", :delayed
+    subscribes :restart, "template[#{node['tomcat-all']['tomcat_home']}/bin/setenv.sh]", :delayed
+    subscribes :restart, "template[#{node['tomcat-all']['tomcat_home']}/conf/server.xml]", :delayed
+  end
 end
